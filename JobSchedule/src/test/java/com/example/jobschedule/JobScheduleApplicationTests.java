28c7248d18075b46ae86d9380a1bf66d63dee4b9
MintChow <120986199@qq.com>
2022-07-27 01:43:22 +0800

    new

--- JobSchedule/src/test/java/com/example/jobschedule/JobScheduleApplicationTests.java
+++ JobSchedule/src/test/java/com/example/jobschedule/JobScheduleApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.jobschedule;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class JobScheduleApplicationTests {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
     @Test
     void contextLoads() {
+        Long aLong = jdbcTemplate.queryForObject("select count(*) from `供水`", Long.class);
+        log.info("记录总数：{}",aLong);
     }
 
 }
