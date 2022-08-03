41c24d349bad0cdc877192b5f80947b495691555
MintChow <120986199@qq.com>
2022-08-04 02:29:11 +0800

    new2

--- JobSchedule/src/test/java/com/example/jobschedule/JobScheduleApplicationTests.java
+++ JobSchedule/src/test/java/com/example/jobschedule/JobScheduleApplicationTests.java
@@ -1,12 +1,21 @@
 package com.example.jobschedule;
 
+import com.example.jobschedule.util.HttpClientUtil;
 import lombok.extern.slf4j.Slf4j;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+
 import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @SpringBootTest
@@ -20,10 +29,23 @@ class JobScheduleApplicationTests {
 
 
     @Test
-    void contextLoads() {
-        Long aLong = jdbcTemplate.queryForObject("select count(*) from `供水`", Long.class);
-        log.info("记录总数：{}",aLong);
-        log.info("数据源:{}",dataSource.getClass());
+    void contextLoads() throws JSONException {
+        Map<String, String> param=new HashMap<>();
+        param.put("fmAddress","1811211494");
+        param.put("startTime","2022-07-29");
+        param.put("endTime","2022-08-04");
+        param.put("interval","1");
+        param.put("analytype","instant");
+        param.put("panel","analysisMeters");
+        String url="http://zsgs.dmas.cn/getData/getFMReadData.ashx";
+        String result=HttpClientUtil.doPost(url,param);
+        JSONArray json =new JSONArray(result);
+
+
+//        Long aLong = jdbcTemplate.queryForObject("select count(*) from `供水`", Long.class);
+//        log.info("记录总数：{}",aLong);
+//        log.info("数据源:{}",dataSource.getClass());
+        System.out.println(json);
 
 
     }
