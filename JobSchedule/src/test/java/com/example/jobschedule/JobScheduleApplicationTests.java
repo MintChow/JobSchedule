6d5892982e2deda33c2b5e71e68c18c86eda00b6
MintChow <120986199@qq.com>
2022-08-05 01:32:37 +0800

    new2

--- JobSchedule/src/test/java/com/example/jobschedule/JobScheduleApplicationTests.java
+++ JobSchedule/src/test/java/com/example/jobschedule/JobScheduleApplicationTests.java
@@ -13,9 +13,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 
 import javax.sql.DataSource;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Slf4j
 @SpringBootTest
@@ -31,21 +32,41 @@ class JobScheduleApplicationTests {
     @Test
     void contextLoads() throws JSONException {
         Map<String, String> param=new HashMap<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String startTime=formatter.format(LocalDateTime.now());
+        String endTime=formatter.format(LocalDateTime.now());
         param.put("fmAddress","1811211494");
-        param.put("startTime","2022-07-29");
-        param.put("endTime","2022-08-04");
+        param.put("startTime",startTime);
+        param.put("endTime",endTime);
         param.put("interval","1");
         param.put("analytype","instant");
         param.put("panel","analysisMeters");
         String url="http://zsgs.dmas.cn/getData/getFMReadData.ashx";
         String result=HttpClientUtil.doPost(url,param);
         JSONArray json =new JSONArray(result);
+        json=json.getJSONArray(0);
+        String arr0;
+        String arr1;
+        ArrayList arrayList = new ArrayList();
+        String min;
+        for (int i=0;i<json.length();i++){
+            arr0=json.getJSONArray(i).getString(0);
+            arr1=json.getJSONArray(i).getString(1);
+
+            arrayList.add(arr1);
+
+//            System.out.println(arr0);
+//            System.out.println(arr1);
+
+        }
+        min= (String) Collections.min(arrayList);
+        System.out.println(min);
 
 
 //        Long aLong = jdbcTemplate.queryForObject("select count(*) from `供水`", Long.class);
 //        log.info("记录总数：{}",aLong);
 //        log.info("数据源:{}",dataSource.getClass());
-        System.out.println(json);
+//        System.out.println(json);
 
 
     }
